1f2c12e26e2b79ff026c9553df14b928cb4447d3
Nik <nikcotta@gmail.com>
2024-04-12 15:10:04 -0700

    added visual loading signifier

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react';
 import Post from '../components/Post';
 import SideNav from '../components/SideNav';
+import CircularProgress from '@mui/material/CircularProgress';
 import '../styles/HomePage.css';
 
 function HomePage() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        fetch(`${import.meta.env.VITE_SERVER_URL}/post`).then(response => {
-            response.json().then(posts => {
-                setPosts(posts);
+        setLoading(true);
+        const fetchPosts = async () => {
+            await fetch(`${import.meta.env.VITE_SERVER_URL}/post`).then(response => {
+                response.json().then(posts => {
+                    setPosts(posts);
+                })
             })
-        })
+        }
+        fetchPosts();
+        setLoading(false);
     }, []);
     
 	return (
 		<div className="homepage-container">
             <SideNav />
             <h3 className="filter">Featured</h3>
+            {loading && (
+                <CircularProgress size="4rem" sx={{ color: "var(--typography-high)" }} />
+            )}
             {posts.length > 0 && posts.map(post => (
                 <Post {...post}/>
             ))}
