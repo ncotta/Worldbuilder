919a6aa23af127a5648d6e8b75dc1a6f3bc5f5bb
Nik <nikcotta@gmail.com>
2025-01-22 22:04:53 -0800

    fix loading spinner

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.css';
+
+function Loading() {
+	return (
+        <div className="spinner-container">
+            <div className="spinner" />
+        </div>
+	)
+}
+
+export default Loading;
