3eb373eeac38d0aea53ef905ef263b28cd3ef492
Nik <nikcotta@gmail.com>
2025-01-22 23:20:21 -0800

    disable map for now

--- client/src/components/Civilizationmap/CivilizationMap.jsx
+++ client/src/components/Civilizationmap/CivilizationMap.jsx
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 function CivilizationMap() {
     return (
         <div style={{ width: '100%', height: '80vh' }}>
-            <Map
+            {/* <Map
                 mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
                 initialViewState={{
                     latitude: 37.7749,
@@ -15,7 +15,7 @@ function CivilizationMap() {
                     width: '100%',
                 }}
                 mapStyle={import.meta.env.VITE_MAPBOX_STYLE}
-            />
+            /> */}
         </div>
     )
 }
